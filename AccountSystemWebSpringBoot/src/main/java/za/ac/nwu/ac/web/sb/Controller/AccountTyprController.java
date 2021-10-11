75ff7b3462b2676260de17289572343200b872a7
Kuhlula <mathebulakuhlula@gmail.com>
2021-10-11 17:31:05 +0200

    first commit

---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/Controller/AccountTyprController.java
@@ -0,0 +1,24 @@
+package za.ac.nwu.ac.web.sb.Controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("account-type")
+public class AccountTyprController {
+
+
+
+}
